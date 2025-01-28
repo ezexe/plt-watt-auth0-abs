207b7d4302988ea9b400b15fc10b225864ba9454
ezexe <elimdadia@gmail.com>
2025-01-28 20:07:53 +0200

    Enhance Auth0 plugin with session creation and pre-handler for user authorization

--- web/db/plugins/auth0.cjs
+++ web/db/plugins/auth0.cjs
@@ -3,19 +3,21 @@
 /** @param {import('fastify').FastifyInstance} fastify */
 module.exports = async function (fastify, opts) {
   const fastifyAuth0Verify = require("fastify-auth0-verify");
-  
+
   // Register the plugin
   fastify.register(fastifyAuth0Verify, {
     domain: opts.auth0.domain, // e.g. "my-tenant.us.auth0.com"
     audience: [opts.auth0.audience, `https://${opts.auth0.domain}/userinfo`],
     formatUser: async (user) => {
-      const dbUser = (await fastify.platformatic.entities.user.find({
-        skipAuth: true,
-        where: {
-          provider: { eq: "auth0" },
-          providerId: { eq: user.sub },
-        },
-      }))[0];
+      const dbUser = (
+        await fastify.platformatic.entities.user.find({
+          skipAuth: true,
+          where: {
+            provider: { eq: "auth0" },
+            providerId: { eq: user.sub },
+          },
+        })
+      )[0];
       return {
         ...user,
         ...dbUser,
@@ -24,5 +26,17 @@ module.exports = async function (fastify, opts) {
         roles: ["user"],
       };
     },
+  });
+
+  fastify.addAuthStrategy({
+    createSession: async (req, reply) => {
+      req.user = await req.jwtVerify();
+    },
+  });
+
+  fastify.addHook("preHandler", async (req, reply) => {
+    if (req.url.startsWith("/user")) {
+      await req.setupDBAuthorizationUser();
+    }
   });
 };
